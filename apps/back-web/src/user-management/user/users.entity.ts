5e2a7b55e2b3d4ffce8033eaad1fdd81cb2ff363
louispelarrey <louispelarrey@gmail.com>
2023-03-26 04:13:01 +0200

    authentication, role based authorization, user management

---
+++ apps/back-web/src/user-management/user/users.entity.ts
@@ -0,0 +1,22 @@
+import { Exclude } from "class-transformer";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Role } from "../role/enums/role.enum";
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column('jsonb', {default: ['USER']})
+  roles: Role[];
+
+  @Column()
+  @Exclude({ toPlainOnly: true })
+  password: string;
+}
