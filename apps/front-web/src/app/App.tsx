640d1c780f44ed58b259bc72fc412d9d4b4aa9a8
louispelarrey <louispelarrey@gmail.com>
2023-03-26 18:32:45 +0200

    fix app.tsx bug

---
+++ apps/front-web/src/app/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <p>Hello worlds</p>
+    </StyledApp>
+  );
+}
+
+export default App;
