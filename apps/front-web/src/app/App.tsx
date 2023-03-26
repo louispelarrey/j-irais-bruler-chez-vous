6dcc00cd4622b2263506dc69b5150e070e99a6ea
louispelarrey <louispelarrey@gmail.com>
2023-03-26 20:28:22 +0200

    frontend: Menu implemented

--- apps/front-web/src/app/App.tsx
+++ apps/front-web/src/app/App.tsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
+import { Menu } from './layouts/Menu/Menu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './containers/Login/Login';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 export function App() {
   return (
     <StyledApp>
-      <CssBaseline />
-      <p>Hello worlds</p>
+      <ThemeProvider theme={darkTheme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <Menu />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </StyledApp>
   );
 }
