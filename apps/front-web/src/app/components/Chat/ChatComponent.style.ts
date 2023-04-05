8f048680be22b6e9166ccbe24bf2300c9084c39f
louispelarrey <louispelarrey@gmail.com>
2023-04-05 16:39:54 +0200

    Chat component implemented

---
+++ apps/front-web/src/app/components/Chat/ChatComponent.style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const StyledChat = styled.div`
+
+  margin-top: 10px;
+
+  .table {
+    min-width: 650px;
+  }
+
+  .headBG {
+    background-color: #e0e0e0;
+  }
+
+  .borderRight500 {
+    border-right: 1px solid gray;
+  }
+
+  .messageArea {
+    height: 70vh;
+    overflow-y: auto;
+  }
+
+  .outlined-basic-email {
+    width: 65%;
+  }
+
+  .interact {
+    padding: 20px 0;
+    display: flex;
+    justify-content: space-evenly;
+    gap: 1px;
+  }
+
+  @media screen and (min-width: 768px) {
+    .outlined-basic-email {
+      width: 85%;
+    }
+  }
+`;
