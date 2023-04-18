34afca937a9f15a32861e514a89fd45b503802ab
louispelarrey <louispelarrey@gmail.com>
2023-04-18 20:05:52 +0200

    frontend: useChat hook

--- apps/front-web/src/app/containers/Chat/Chat.tsx
+++ apps/front-web/src/app/containers/Chat/Chat.tsx
@@ -1,86 +1,14 @@
-import { useState, useEffect, useCallback, useContext, useMemo, useRef } from 'react';
-import io, { Socket } from 'socket.io-client';
 import { ChatComponent } from '../../components/Chat/ChatComponent';
-import { useForm } from 'react-hook-form';
-import { UserContext } from '../../contexts/UserContext';
-import getUserIdFromToken from '../../utils/user/getUserIdFromToken';
-import { transformApiResponse } from '../../utils/message/transformApiResponse';
+import { useChat } from '../../hooks/useChat';
 
-export interface ChatForm {
-  newMessage: string;
+export interface ChatProps {
+  roomName?: string;
 }
 
-export interface Message {
-  username: string;
-  userId: string;
-  message: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface ApiMessage {
-  id: string;
-  message: string;
-  createdAt: string;
-  updatedAt: string;
-  sender: User;
-}
-
-interface User {
-  id: string;
-  username: string;
-  email: string;
-  roles: UserRole[];
-  password: string;
-}
-
-type UserRole = 'USER';
-
-export const Chat = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const {register, handleSubmit} = useForm<ChatForm>();
-  const {token} = useContext(UserContext);
-  const userId = getUserIdFromToken(token);
-  const roomName = 'default';
-  const socket = useMemo(() => {
-    // Socket.io instanciation
-    return io(':3001',
-      {
-        query: {
-          roomName,
-        },
-      });
-  }, [roomName]);
-
-  const onNewMessage = useCallback((messages: ApiMessage | ApiMessage[]) => {
-    const transformedMessages = transformApiResponse(messages);
-    Array.isArray(transformedMessages) ? setMessages(transformedMessages) :
-      setMessages((messages) => [...messages, transformedMessages]);
-  }, []);
-
-  const sendMessage = useCallback(({newMessage}: ChatForm) => {
-    socket && socket.emit('createMessage', {
-      senderId: userId,
-      message: newMessage,
-      roomName,
-    });
-  }, [socket, userId]);
-
-  useEffect(() => {
-    socket.on('newMessage', onNewMessage);
-
-    return () => {
-      socket.off('newMessage', onNewMessage);
-    };
-  }, [onNewMessage, socket]);
-
-  const scrollTarget = useRef<any>(null);
-
-  useEffect(() => {
-    if (scrollTarget.current) {
-      scrollTarget.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [messages.length]);
+export const Chat = ({ roomName = 'default' }: ChatProps) => {
+  const { messages, userId, scrollTarget, handleSubmit, register, sendMessage } = useChat({
+    roomName,
+  });
 
   return (
     <ChatComponent
