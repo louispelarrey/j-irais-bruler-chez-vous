4dda3c898d80e51f15f0714b1893b055bb32e65d
calintha <cal.intha77@gmail.com>
2023-04-20 14:18:52 +0200

    frontend: get trash list

--- apps/front-web/src/app/components/Shared/CardComponent.tsx
+++ apps/front-web/src/app/components/Shared/CardComponent.tsx
@@ -1,29 +1,38 @@
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Grid } from '@mui/material';
 
-export default function CardComponent() {
+interface CardComponentProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export const CardComponent = ({
+  title,
+  description,
+  image,
+}: CardComponentProps) => {
   return (
-    <Grid container spacing={2}>
-      <Card sx={{ maxWidth: 345 }}>
+    <Grid item xs={12} sm={6} md={4} sx={{ mb: 3 }}>
+      <Card sx={{ height: '100%' }}>
         <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
+          component="img"
+          height="140"
+          image={image}
+          alt="image"
         />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  </Grid>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Voir</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
