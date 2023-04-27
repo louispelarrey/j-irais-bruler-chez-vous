4fca75024f45da7541016144f788e84aeb4be1d3
louispelarrey <louispelarrey@gmail.com>
2023-04-28 00:09:07 +0200

    Message microservice connected, started implementing it

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthenticationModule } from '../authentication/authentication.module';
 import { UsersModule } from '../user/users.module';
-import { MessageModule } from '../message/message.module';
 import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 import { RoleGuard } from '../user/role/guard/role.guard';
+import { MessageModule } from '../message/message.module';
 
 @Module({
   imports: [
--- apps/api-gateway/src/message/message.service.ts
+++ apps/api-gateway/src/message/message.service.ts
@@ -1,11 +1,31 @@
+import { MessageDto } from '@j-irais-bruler-chez-vous/message/feature';
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class MessageService {
-  constructor(@Inject('MESSAGE') private readonly messageClient: ClientProxy) {}
 
-  create(body: any) {
-    return this.messageClient.send('message', body)
+  constructor(
+    @Inject('MESSAGE') private readonly messageClient: ClientProxy,
+  ){}
+
+  findAllByRoom(roomName: string) {
+    return this.messageClient.send('findAllByRoom', roomName);
+  }
+
+  create(messageDto: MessageDto) {
+    return this.messageClient.send('create', messageDto);
+  }
+
+  findOne(id: string) {
+    return this.messageClient.send('findOne', id);
+  }
+
+  update(id: string, messageDto: MessageDto) {
+    return this.messageClient.send('update', { id, messageDto });
+  }
+
+  remove(id: string) {
+    return this.messageClient.send('remove', id);
   }
 }
