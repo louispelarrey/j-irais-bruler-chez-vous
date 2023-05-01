992bc04d49f5e9bdc2491b686666b939ba1ab786
louispelarrey <louispelarrey@gmail.com>
2023-05-01 15:50:21 +0200

    public health check fix

--- apps/api-gateway/src/app/app.controller.ts
+++ apps/api-gateway/src/app/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../authentication/decorators/public.decorator';
 
 @Controller()
 export class AppController {
 
   @Get()
+  @Public()
   health(): boolean {
     return true;
   }
