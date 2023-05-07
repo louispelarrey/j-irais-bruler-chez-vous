85028bb2f8894511576170e22a823e84154e9826
calintha <cal.intha77@gmail.com>
2023-05-07 15:18:11 +0200

    add trash front

--- apps/front-web/src/app/containers/Trash/List.tsx
+++ apps/front-web/src/app/containers/Trash/List.tsx
@@ -2,9 +2,9 @@ import { CardComponent } from "../../components/Shared/CardComponent";
 import { Grid } from '@mui/material';
 import useGet from "../../hooks/useGet";
 
-interface Trash {
+interface List {
     id: string;
-    name: string;
+    reference: string;
     description: string;
 }
 
@@ -21,10 +21,10 @@ export const Trashs = () => {
 
     return (
         <Grid container spacing={2} sx={{ maxWidth: '1280px', mx: 'auto', px: 2 , my: 2}}>
-            {data.map((item: Trash) => (
+            {data.map((item: List) => (
                 <CardComponent
                     key={item.id}
-                    title={item.name}
+                    title={item.reference}
                     description={item.description}
                     image="https://images.unsplash.com/photo-1616489953149-8e1b0e1b5f0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
                     trashId={item.id}
