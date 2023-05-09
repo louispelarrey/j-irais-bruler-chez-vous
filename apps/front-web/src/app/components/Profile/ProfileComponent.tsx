9a604f02219a833b0a80a2866ad48194efb40029
louispelarrey <louispelarrey@gmail.com>
2023-05-09 14:59:26 +0200

    created trash prod conf

---
+++ apps/front-web/src/app/components/Profile/ProfileComponent.tsx
@@ -0,0 +1,7 @@
+export const ProfileComponent = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+    </div>
+  )
+};
---
+++ apps/front-web/src/app/containers/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import { ProfileComponent } from "../../components/Profile/ProfileComponent";
+import useGet from "../../hooks/useGet";
+
+export const Profile = () => {
+
+  return (
+    <ProfileComponent />
+  );
+};
