a0670a652c29f6adb77afb2086c699731c66b66b
calintha <cal.intha77@gmail.com>
2023-05-10 11:42:56 +0200

    add posterId to trash api

--- apps/api-trash/src/trash/trash.module.ts
+++ apps/api-trash/src/trash/trash.module.ts
@@ -3,10 +3,21 @@ import { TrashService } from './trash.service';
 import { TrashController } from './trash.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Trash } from './trash.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Trash]),
+    ClientsModule.register([
+      {
+        name: 'USER',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.TCP_USER_HOST,
+          port: 3001,
+        },
+      },
+    ])
   ],
   controllers: [TrashController],
   providers: [TrashService]
