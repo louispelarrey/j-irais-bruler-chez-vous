52ac561dd6f6fbeae3e00630827513ad33dc27c6
louispelarrey <louispelarrey@gmail.com>
2023-05-10 13:00:51 +0200

    fix authentication

--- apps/api-gateway/src/authentication/authentication.module.ts
+++ apps/api-gateway/src/authentication/authentication.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy'; // adjust the path according to your project structure
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
+    ConfigModule, // import ConfigModule here
     ClientsModule.register([
       {
         name: 'AUTHENTICATION',
@@ -12,11 +18,20 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         options: {
           host: process.env.TCP_AUTHENTICATION_HOST,
           port: 3000,
-        }
+        },
       },
-    ])
+    ]),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // also add ConfigModule here
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: '3600s' },
+      }),
+    }),
   ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
 })
 export class AuthenticationModule {}
