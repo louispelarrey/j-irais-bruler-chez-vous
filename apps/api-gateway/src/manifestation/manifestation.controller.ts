ef4953c39775933ef247f775b63d831c95323912
calintha <cal.intha77@gmail.com>
2023-05-22 22:25:11 +0200

    add join manif

--- apps/api-gateway/src/manifestation/manifestation.controller.ts
+++ apps/api-gateway/src/manifestation/manifestation.controller.ts
@@ -2,14 +2,15 @@ import {Body, Controller, Get, Param, Post, Put, Request} from '@nestjs/common';
 import {ManifestationService} from "./manifestation.service";
 import {CreateManifestationDto} from "./dto/create-manifestation.dto";
 import {UpdateManifestationDto} from "./dto/update-manifestation";
+import { Public } from '../authentication/decorators/public.decorator';
 
 
 @Controller('manifestation')
 export class ManifestationController {
   constructor(private readonly manifestationService: ManifestationService) {}
 
-
   @Get()
+  @Public()
   findAll() {
     return this.manifestationService.findAll();
   }
