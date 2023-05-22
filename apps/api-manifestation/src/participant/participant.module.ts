460ea1eec0fa3bff4db75efb034f706d63f9ea83
calintha <cal.intha77@gmail.com>
2023-05-22 22:26:43 +0200

    add join manif to api manifestation

--- apps/api-manifestation/src/participant/participant.module.ts
+++ apps/api-manifestation/src/participant/participant.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ParticipantService } from './participant.service';
 import { ParticipantController } from './participant.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Participant } from './participant.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Participant])
+  ],
   controllers: [ParticipantController],
   providers: [ParticipantService]
 })
