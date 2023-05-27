2488832abbeeb43a0d2846043c9aa7ea0e0ee871
louispelarrey <louispelarrey@gmail.com>
2023-05-27 18:54:13 +0200

    small login fix

--- apps/front-web/src/app/containers/Login/Login.tsx
+++ apps/front-web/src/app/containers/Login/Login.tsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import { useForm } from "react-hook-form";
-import { Navigate, useNavigate } from "react-router-dom";
-import { LoginComponent } from "../../components/Login/LoginComponent";
-import { UserContext } from "../../contexts/UserContext";
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { LoginComponent } from '../../components/Login/LoginComponent';
+import { UserContext } from '../../contexts/UserContext';
 
 export interface LoginData {
   email: string;
@@ -11,29 +11,42 @@ export interface LoginData {
 
 export const Login = () => {
   const { register, handleSubmit } = useForm<LoginData>();
+  const [error, setError] = useState('');
   const { token, setToken } = useContext(UserContext);
   const navigate = useNavigate();
 
   const onSubmit = async ({ email, password }: LoginData) => {
     const response = await fetch(
-      import.meta.env.VITE_APP_BACKEND_URL + "/api/authentication/login",
+      import.meta.env.VITE_APP_BACKEND_URL + '/api/authentication/login',
       {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           username: email,
-          password
+          password,
         }),
-    });
+      }
+    );
+
     const data = await response.json();
-    if(data.access_token) {
-      setToken(data.access_token);
+    if (response.status !== 201 || !data) {
+      setError('Email ou mot de passe incorrect');
+      return;
     }
 
-    navigate("/posting");
-  }
+    if (data.access_token) {
+      setToken(data.access_token);
+      navigate('/posting');
+    }
+  };
 
-  if(token) return <Navigate to="/" replace />;
+  if (token) return <Navigate to="/" replace />;
 
-  return <LoginComponent handleSubmit={handleSubmit(onSubmit)} register={register} />;
+  return (
+    <LoginComponent
+      handleSubmit={handleSubmit(onSubmit)}
+      register={register}
+      error={error}
+    />
+  );
 };
