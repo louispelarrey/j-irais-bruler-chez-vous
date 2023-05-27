d5d6c0a4bfe9cb4fa383725f221bac21e30ef05a
louispelarrey <louispelarrey@gmail.com>
2023-05-27 19:59:39 +0200

    trash map front

--- apps/front-web/src/app/components/Trash/Modal/TrashModalComponent.tsx
+++ apps/front-web/src/app/components/Trash/Modal/TrashModalComponent.tsx
@@ -1,9 +1,12 @@
 import { Box, Button, Grid, Paper, TextField, Typography } from '@mui/material';
 import { UseFormRegister } from 'react-hook-form';
+import { MapComponent } from '../../Map/MapComponent';
+import { useState } from 'react';
 
 export interface TrashData {
   reference: string;
   description: string;
+  address: string;
 }
 
 interface TrashModalProps {
@@ -15,6 +18,9 @@ export const TrashModalComponent = ({
   handleSubmit,
   register,
 }: TrashModalProps) => {
+
+  const [address, setAddress] = useState('');
+
   return (
     <Grid container component="main">
       <Grid item component={Paper} elevation={6} square>
@@ -36,13 +42,25 @@ export const TrashModalComponent = ({
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Créer une annonce
             </Typography>
+            <MapComponent setAddress={setAddress}/>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Adresse"
+              type="address"
+              id="address"
+              value={address}
+              autoComplete="address"
+              {...register('address')}
+            />
             <TextField
               margin="normal"
               required
               fullWidth
               id="reference"
               type="reference"
-              label="Référence"
+              label="Titre"
               autoComplete="reference"
               {...register('reference')}
               autoFocus
