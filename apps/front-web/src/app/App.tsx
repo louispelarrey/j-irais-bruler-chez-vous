5c0d020299bc31adf269df159001a78a8fa528a3
louispelarrey <louispelarrey@gmail.com>
2023-06-25 17:19:44 +0200

    changed font

--- apps/front-web/src/app/App.tsx
+++ apps/front-web/src/app/App.tsx
@@ -5,12 +5,12 @@ import { Menu } from './layouts/Menu/Menu';
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import { SuspenseLoader } from './suspense/SuspenseLoader';
+import FontMedium from './fonts/font-medium.woff2';
 
 const Login = lazy(() => import('./containers/Login/Login').then(module => ({ default: module.Login })));
 const Logout = lazy(() => import('./containers/Logout/Logout').then(module => ({ default: module.Logout })));
 const Protected = lazy(() => import('./containers/Protected/Protected').then(module => ({ default: module.Protected })));
 const Register = lazy(() => import('./containers/Register/Register').then(module => ({ default: module.Register })));
-const Chat = lazy(() => import('./containers/Chat/Chat').then(module => ({ default: module.Chat })));
 const ShowSpecific = lazy(() => import('./containers/Trash/ShowSpecific').then(module => ({ default: module.ShowSpecific })));
 const ListTrash = lazy(() => import('./containers/Trash/List').then(module => ({ default: module.Trashs })));
 
@@ -22,11 +22,26 @@ const StyledApp = styled.div`
   height: calc(100% - env(safe-area-inset-top) - env(safe-area-inset-bottom));
 `;
 
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
+  typography: {
+    fontFamily: 'FontMedium',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'FontMedium';
+          src: local('FontMedium'), local('FontMedium-Regular'), url(${FontMedium}) format('woff2');
+        }
+      `,
+    },
+  },
 });
+
 
 export function App() {
   return (
@@ -39,7 +54,6 @@ export function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/chat" element={<Protected><Chat /></Protected>} />
             <Route path="/posting" element={<Protected><ListTrash/></Protected>} />
             <Route path="/posting/:id" element={<Protected><ShowSpecific/></Protected>} />
             <Route path="*" element={<div>404</div>} />
