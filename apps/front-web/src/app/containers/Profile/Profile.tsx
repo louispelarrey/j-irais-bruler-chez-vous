cc211d37259f0e41048d3ec36dd854fe5669e064
calintha <cal.intha77@gmail.com>
2023-06-27 14:58:04 +0200

    add modal update user

--- apps/front-web/src/app/containers/Profile/Profile.tsx
+++ apps/front-web/src/app/containers/Profile/Profile.tsx
@@ -2,6 +2,9 @@ import { useCallback, useState } from "react";
 import { ProfileComponent } from "../../components/Profile/ProfileComponent";
 import getUserIdFromToken from '../../utils/user/getUserIdFromToken';
 import useGet from '../../hooks/useGet';
+import { useNavigate } from 'react-router-dom';
+import { SuspenseLoader } from '../../suspense/SuspenseLoader';
+import { useForm } from 'react-hook-form';
 
 interface UserData {
   email: string;
@@ -11,9 +14,46 @@ interface UserData {
 export const Profile = () => {
   const userId = getUserIdFromToken(localStorage.getItem('token') ?? '');
   const { data, error, loading } = useGet(`/api/users/${userId}`);
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<UserData>();
+
+  const onSubmit = async ({
+    username
+  }: UserData) => {
+
+    const formData = new FormData();
+    formData.append('username', username);
+
+    const response = await fetch(`/api/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: formData,
+    });
+    const data = await response.json();
+
+    if (data.statusCode === 401) {
+      // navigate('/logout');
+      navigate(`/users/${userId}`);
+    }
+    if (data.id) {
+      navigate(`/users/${userId}`);
+    }
+  };
+
+  if (loading) {
+    return <SuspenseLoader children={<></>} />;
+  }
+  if (error) {
+    return <div>Erreur</div>;
+  }
+
   return (
-    <div>
-      <ProfileComponent data={data} />
-    </div>
+    <ProfileComponent
+      data={data}
+      register={register}
+      onSubmit={handleSubmit(onSubmit)}
+    />
   );
 };
