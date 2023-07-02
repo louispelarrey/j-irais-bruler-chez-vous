eb6f59a92ba9e36dd73a6c52426703f95e6e9b3b
louispelarrey <louispelarrey@gmail.com>
2023-07-02 23:08:06 +0200

    enable compression

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app/app.module';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
 
   app.enableCors({origin: [process.env.FRONTEND_URL, 'capacitor://localhost'] });
   app.use(helmet());
+  app.use(compression());
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
