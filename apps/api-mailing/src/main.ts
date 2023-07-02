f2eecd7eee5dcddf766c5e1a875b8341907a32c2
louispelarrey <louispelarrey@gmail.com>
2023-07-02 23:08:06 +0200

    enable compression

--- apps/api-mailing/src/main.ts
+++ apps/api-mailing/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
@@ -16,7 +15,6 @@ async function bootstrap() {
     }
   })
   await app.listen();
-
 }
 
 bootstrap();
