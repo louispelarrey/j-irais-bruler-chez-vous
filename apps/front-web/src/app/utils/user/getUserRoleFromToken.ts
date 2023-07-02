ea34fc726dd3ca3b14c5369d4cf87bafb60129f3
louispelarrey <louispelarrey@gmail.com>
2023-07-02 22:51:47 +0200

    no any in nest.js

--- apps/front-web/src/app/utils/user/getUserRoleFromToken.ts
+++ apps/front-web/src/app/utils/user/getUserRoleFromToken.ts
@@ -1,8 +1,14 @@
 import jwt_decode from 'jwt-decode';
 
+interface DecodedToken {
+  sub: string;
+  username: string;
+  roles: string;
+}
+
 const getUserRoleFromToken = (token: string): string | null => {
     try {
-        const decoded: any = jwt_decode(token);
+        const decoded: DecodedToken = jwt_decode(token);
         return decoded.roles;
     } catch (error) {
         console.error('Error decoding token:', error);
