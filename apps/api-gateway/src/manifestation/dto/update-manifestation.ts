95430a71899eb22f529f1219a74313035b600e90
Sadik Raida <sadik.raida@gmail.com>
2023-07-05 14:59:43 +0200

    manifestation validation

--- apps/api-gateway/src/manifestation/dto/update-manifestation.ts
+++ apps/api-gateway/src/manifestation/dto/update-manifestation.ts
@@ -1,6 +1,16 @@
+import { IsString } from 'class-validator';
+
 export class UpdateManifestationDto {
+  
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsString()
   address: string;
+
+  @IsString()
   start_date: string;
 }
