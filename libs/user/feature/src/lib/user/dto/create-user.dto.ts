8a3b7f375495e123b0ed6a78504b476c0a5228dd
Sadik Raida <sadik.raida@gmail.com>
2023-07-05 14:37:34 +0200

    user validation

--- libs/user/feature/src/lib/user/dto/create-user.dto.ts
+++ libs/user/feature/src/lib/user/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsString } from "class-validator";
 
 export class CreateUserDto {
+  @IsString()
   username: string;
 
   @IsEmail()
   email: string;
 
+  @IsString()
   password: string;
 }
--- libs/user/feature/src/lib/user/dto/update-user.dto.ts
+++ libs/user/feature/src/lib/user/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsString } from "class-validator";
+
 export class UpdateUserDto {
+
+  @IsString()
   username: string;
 }
 
