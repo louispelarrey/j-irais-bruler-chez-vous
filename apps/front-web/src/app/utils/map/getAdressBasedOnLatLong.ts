5f1e9c383fa2e606c17d77e99b2dc8f7976f778f
louispelarrey <louispelarrey@gmail.com>
2023-07-06 23:26:29 +0200

    changed getaddress behavior

--- apps/front-web/src/app/utils/map/getAdressBasedOnLatLong.ts
+++ apps/front-web/src/app/utils/map/getAdressBasedOnLatLong.ts
@@ -3,7 +3,18 @@ const getAdressBasedOnLatLong = async (lat: number, long: number) => {
     `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=jsonv2`
   );
   const data = await response.json();
-  return `${data.address.house_number} ${data.address.road}, ${data.address.postcode} ${data.address.city}`;
+
+  if (
+    data.address &&
+    data.address.house_number &&
+    data.address.road &&
+    data.address.postcode &&
+    data.address.city
+  ) {
+    return `${data.address.house_number} ${data.address.road}, ${data.address.postcode} ${data.address.city}`;
+  } else {
+    return data.display_name;
+  }
 };
 
 export default getAdressBasedOnLatLong;
