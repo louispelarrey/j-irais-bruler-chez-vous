2c37eb811e02f0c5fdc265feab690496bf207934
calintha <cal.intha77@gmail.com>
2023-07-06 11:22:32 +0200

    fix login errors

--- apps/front-web/src/app/containers/Login/Login.tsx
+++ apps/front-web/src/app/containers/Login/Login.tsx
@@ -17,32 +17,36 @@ export const Login = () => {
   const navigate = useNavigate();
 
   const onSubmit = async ({ email, password }: LoginData) => {
-    const response = await fetch(
-      import.meta.env.VITE_APP_BACKEND_URL + '/api/authentication/login',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          username: email,
-          password,
-        }),
-      }
-    );
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_APP_BACKEND_URL + '/api/authentication/login',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            username: email,
+            password,
+          }),
+        }
+      );
 
-    const data = await response.json();
-    if (response.status !== 201 || !data) {
-      setError('Email ou mot de passe incorrect');
-      return;
-    }
-
-    if (data.access_token) {
-      setToken(data.access_token);
-      const role = getUserRoleFromToken(localStorage.getItem('token') ?? '');
-      if (role?.includes('ADMIN')) {
-        navigate('/dashboard');
-      } else {
-        navigate('/');
+      const data = await response.json();
+      if (response.status !== 201 || !data) {
+        setError('Email ou mot de passe incorrect');
+        return;
       }
+
+      if (data.access_token) {
+        setToken(data.access_token);
+        const role = getUserRoleFromToken(localStorage.getItem('token') ?? '');
+        if (role?.includes('ADMIN')) {
+          navigate('/dashboard');
+        } else {
+          navigate('/');
+        }
+      }
+    } catch (e: any) {
+      setError(e.message);
     }
   };
 
