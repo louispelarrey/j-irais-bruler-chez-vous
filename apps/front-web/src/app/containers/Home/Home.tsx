4e7640ba4d0dbf0ef2a01f3365bf0e1efa3d6dda
louispelarrey <louispelarrey@gmail.com>
2023-07-06 23:19:04 +0200

    deleted unused fragment

--- apps/front-web/src/app/containers/Home/Home.tsx
+++ apps/front-web/src/app/containers/Home/Home.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BannerFragment } from './fragments/BannerFragment';
 import { ListFragment } from './fragments/ListFragment';
-import { WorksFragment } from './fragments/WorksFragment';
 import { Heatmap } from './HeatMap/Heatmap';
 
 export const Home = () => {
